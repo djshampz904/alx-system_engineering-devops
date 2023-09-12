3ef371436c51726ea8a10371c670700991775607
djshampz904 <djshampz05@gmail.com>
2023-09-12 05:42:02 +0000

    added more validation

--- 0x0A1/1-imageparse/testing/challenge/challenge.sh
+++ 0x0A1/1-imageparse/testing/challenge/challenge.sh
@@ -1,13 +1,38 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+NC='\033[0m'
+zip_file=0
+
+OPTERR="${RED}Invalid option or missing argument error - exiting ...${NC}"
+
+while getopts ':z' opt
+do
+	case $opt in
+		z) zip_file=1;;
+
+		:) zip_file=0;;
+
+		*) echo -e "${OPTERR}"
+		   exit 1;;
+	esac
+done
+
+if [ $# -gt $((OPTIND - 1)) ]; then
+	echo -e "${OPTERR}"
+	exit 1
+fi
+
 read -p "Enter a URL and image file type, e.g. http://somedomain.com jpg: " url imagetype
 	
 if [ -z "$imagetype" ] || [ -z "$url" ]; then
-	echo "One or more arguments have not been provided.Exiting..."
+	echo "${RED}One or more arguments have not been provided.Exiting...${NC}"
 	exit 1
 
 elif ! [[ "$imagetype" == "jpg" || "$imagetype" == "jpeg" || "$imagetype" = "gif" || "$imagetype" = "png" ]]; then
-        echo "Unsupported image type entered. Exiting..."
+        echo "${RED}Unsupported image type entered. Exiting...${NC}"
 	exit 1
 fi
 
@@ -16,10 +41,9 @@ echo -e "Scanning for $imagetype at $url"
 current_datetime=$(date +'%Y_%m_%d_%H_%M')
 my_folder="${imagetype}_${current_datetime}"
 
-
 while IFS= read -r link;
 do
 
-	wget "$link" -P "$my_folder"
+	wget -q "$link" -P "$my_folder"
 
 done < <(curl -s "$url" | grep -Eo '(http|https)://[^"]+' | grep -o "https://[^\"]*\.$imagetype")
