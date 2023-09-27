5bc41a7f2798bbcbd208830e57f9332a45936607
djshampz904 <djshampz05@gmail.com>
2023-09-27 16:06:05 +0000

    magic

--- 0x0A1/1-imageparse/imgparse_pro/getimgsproV2.sh
+++ 0x0A1/1-imageparse/imgparse_pro/getimgsproV2.sh
@@ -30,6 +30,7 @@ current_datetime=""
 my_folder=""
 download_count=0
 img_types=("jpg" "jpeg" "gif" "png")
+alltype=0
 
 # Function to format file size in human-readable format
 getsize()
@@ -48,6 +49,11 @@ getsize()
     fi
 }
 
+get_dirs()
+{
+
+}
+
 # Function to download an image
 dwn_image()
 {
@@ -63,25 +69,40 @@ dwn_image()
 # Function to fetch image links from the provided URL
 get_img_links()
 {
-    # Use curl to fetch the HTML content, then grep for image links only returning links that end with "." and the image type
-    curl -s "$url" | grep -Eo "https://[^\"]*\.$imagetype" | sed "s/<[^>]\+//g"
+		# Use curl to fetch the HTML content, then grep for image links only returning links that end with "." and the image type
+		curl -s "$url" | grep -Eo "https://[^\"]*\.$imagetype" | sed "s/<[^>]\+//g"
 }
 
 # Check if zip_file is not 1, which means -z was not provided
 if [[ $# -gt 0 ]]; then
-	while getopts ":z" opt;
+	while getopts ":za" opt;
 	do
 		case $opt in
 			z) zip_file=1;;
+			a) alltype=1
+				imagetype="("
+				# Loop through the array and construct the string
+				for type in "${img_types[@]}";
+				do
+					# Add each element to the string with '|' as a separator
+					imagetype+="$type|"
+				done
+				# Remove the trailing '|' character from the string
+				imagetype=${imagetype%|}
+				imagetype+=")"
+				;;
+
 			*) printf "$OPTERR\n"; exit 1;;
 		esac
 	done
 fi
 
+echo "$imagetype";
+
 args=()
 #Get any other argument provided thats not -z and put it in an array
 for arg in "$@"; do
-	if [ "$arg" != "-z" ]; then
+	if [ "$arg" != "-z" ] && [ "$arg" != "-a" ]; then
 		args+=("$arg")
 	fi
 done
@@ -91,23 +112,33 @@ if [ ${#args[@]} -gt 0 ]; then
 	printf "${OPTERR}\n"; exit 1
 fi
 
-# Prompt the user for the URL and image type
-read -p "Enter a URL and image file type, e.g. http://somedomain.com jpg: " url imagetype
+if ! [ $alltype -eq 1 ]; then
+	# Prompt the user for the URL and image type
+	read -p "Enter a URL and image file type, e.g. http://somedomain.com jpg: " url imagetype
 
-# Trim leading/trailing whitespace from user input
-url=$(echo "$url" | sed 's/^[ \t]*//;s/[ \t]*$//')
-imagetype=$(echo "$imagetype" | sed 's/^[ \t]*//;s/[ \t]*$//')
+	url=$(echo "$url" | sed 's/^[ \t]*//;s/[ \t]*$//')
+	imagetype=$(echo "$imagetype" | sed 's/^[ \t]*//;s/[ \t]*$//')
+else
+	read -p "Enter a URL e.g. http://somedomain.com: " url
+
+	url=$(echo "$url" | sed 's/^[ \t]*//;s/[ \t]*$//')
+	imagetype=$imagetype
+fi
 
 # Validate user input and check for url and imagetype if they are present if not exit showing error
 if [ -z "$imagetype" ] || [ -z "$url" ]; then
     printf "${RED}One or more arguments have not been provided. Exiting...${NC}\n"
     exit 1
-elif ! [[ "${img_types[*]}" == *"$imagetype"* ]]; then
+elif ! [[ "${img_types[*]}" == *"$imagetype"* ]] && ! [[ $alltype -eq 1 ]]; then
     printf "${RED}Unsupported image type entered. Exiting...${NC}\n"
     exit 1
 fi
 
-printf "Scanning for $imagetype images at $url\n"
+if ! [ $alltype -eq 1 ];then
+	printf "Scanning for $imagetype images at $url\n"
+else
+	printf "Scanning for $imagetype\n"
+fi
 
 # Call the method for getting links from url provided 
 links=$(get_img_links)
@@ -127,11 +158,33 @@ printf "$img_found ${GREEN}$imagetype${NC} files detected at URL, which include 
 
 # Create a unique directory for storing downloaded images
 current_datetime=$(date +'%Y_%m_%d_%H_%M_%S') #Use date time to the second so to always generate unique name of file
-my_folder="${imagetype}_${current_datetime}" #create folder name
+if ! [ $alltype -eq 1 ]; then
+	my_folder="${imagetype}_${current_datetime}" #create folder name
+else
+	jpg_folder="jpg_${current_datetime}"
+	gif_folder="gif_${current_datetime}"
+	jpeg_folder="jpeg_${current_datetime}"
+	png_folder="png_${current_datetime}"
+fi
+
 
 # Download unique images
 for link in ${unique_links}; do
-    dwn_image "$link" "$my_folder"
+	if ! [ $alltype -eq 1 ]; then
+		dwn_image "$link" "$my_folder"
+	else
+		case "$link" in
+			*.jpg) dwn_image "$link" "$jpg_folder"
+				;;
+			*.jpeg) dwn_image "$link" "$jpeg_folder"
+				;;
+			*.gif)dwn_image "$link" "$gif_folder"
+				;;
+			*.png)dwn_image "$link" "$png_folder"
+				;;
+        esac
+
+	fi
 done
 
 printf "$download_count ${GREEN}$imagetype${NC} files have been downloaded to the directory ${GREEN}$my_folder${NC}\n"
@@ -141,19 +194,48 @@ printf "${BLUE}FILE NAME                                          FILE SIZE${NC}
 printf "%-50s %-20s\n" "--------------------------------------------------" "--------------------"
 
 #loop through the created directory and select file and file size
-for file in "$my_folder"/*; do
-    fname=$(basename "$file")
-    if [ ${#fname} -gt 30 ]; then #If the name is longer than 30 characters trim it down to 30 fit the table
-        fname=${fname:0:27}"..." 
-    fi
-    fsize=$(getsize $(du -b "$file" | cut -f 1))
-    printf "%-50s |  %-20s\n" "$fname" "$fsize"
-done
+if ! [ $alltype -eq 1 ]; then
+	for file in "$my_folder"/*; do
+		fname=$(basename "$file")
+		if [ ${#fname} -gt 30 ]; then #If the name is longer than 30 characters trim it down to 30 fit the table
+			fname=${fname:0:27}"..." 
+		fi
+		fsize=$(getsize $(du -b "$file" | cut -f 1))
+		printf "%-50s |  %-20s\n" "$fname" "$fsize"
+	done
+else
+	for folder_name in "$jpg_folder" "$gif_folder" "$jpeg_folder" "$png_folder";
+	do
+		if [ -d "$folder_name" ]; then
+			for file in "$folder_name"/*;
+			do
+				fname=$(basename "$file")
+				if [ ${#fname} -gt 30 ]; then
+					fname=${fname:0:27}"..."
+				fi
+				fsize=$(getsize $(du -b "$file" | cut -f 1))
+				printf "%-50s |  %-20s\n" "$fname" "$fsize"
+			done
+		fi
+	done
+fi
 
 if [ ${zip_file} -eq 1 ]; then
-    # Create a zip archive of downloaded images
-    zip -q -r "$my_folder/$my_folder.zip" "$my_folder"
-    printf "${GREEN}$download_count${NC} $imagetype files archived to ${GREEN}$my_folder.zip${NC} in the ${GREEN}$my_folder${NC} directory\n"
-fi 
+	if ! [ $alltype -eq 1 ]; then
+		# Create a zip archive of downloaded images
+		zip -q -r "$my_folder/$my_folder.zip" "$my_folder"
+		printf "${GREEN}$download_count${NC} $imagetype files archived to ${GREEN}$my_folder.zip${NC} in the ${GREEN}$my_folder${NC} directory\n"
+	else
+		for folder_name in "$jpg_folder" "$gif_folder" "$jpeg_folder" "$png_folder";
+		do
+			if [ -d "$folder_name" ]; then
+				folder_type=$(basename "$folder_name")
+				zip -q -r "$folder_name/$folder_name.zip" "$folder_name"
+				printf "${GREEN}$download_count${NC} $folder_type files archived to ${GREEN}$folder_name.zip${NC} in the ${GREEN}$folder_name${NC} directory\n"
+
+			fi
+		done
+	fi
+fi	
 
 exit 0
