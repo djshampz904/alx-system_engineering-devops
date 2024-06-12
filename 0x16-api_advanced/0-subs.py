add834aebb2ce87a3d3a1c28bfc8e328ecd74a42
djshampz904 <martin.kimani@strathmore.edu>
2024-06-12 15:00:22 +0300

    ssh configuration

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,16 @@
 #!/usr/bin/python3
-"""
-Query reddit users and total  subscribers
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Query reddit users and total  subscribers
-    """
-    import requests
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': 'Mozilla/5.0'
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        return response.json().get('data').get('subscribers')
-    else:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
